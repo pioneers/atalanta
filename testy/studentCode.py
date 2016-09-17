a43b101b157b7dacf373d7449bad757ae853563d
Legeng Liu <legengliu@berkeley.edu>
2016-09-17 14:48:46 -0700

    fixed 2

--- testy/studentCode.py
+++ testy/studentCode.py
@@ -10,13 +10,9 @@ def setup(pipe):
 def main(stateQueue, pipe):
   # while True:
   #   pass
-<<<<<<< HEAD
-  state
-=======
   stateQueue.put([SM_COMMANDS.GET_VAL])
   response = pipe.recv()
   print("State Info:", response)
->>>>>>> origin/devel/justin
   print("Saying hello to the other side")
   state = Robot.getValue("")
   print("Hello from the other side %d" % (state,))
